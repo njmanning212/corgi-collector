8ed46baf939932c2b3c44ffe5ccd729008da0a8c
Nicholas Manning <njmanning212@gmail.com>
2023-05-26 13:36:17 -0500

    add corgi index url, view, template, and css

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('corgis/', views.corgis_index, name='cargi-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class Corgi:
+  def __init__(self, name, type, description, age):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.age = age
+
+corgis = [
+  Corgi('Toffee', 'Pembroke Welsh', 'All about the foot', 4),
+  Corgi('Bilbo', 'Pembroke Welsh', 'The best herder', 3),
+  Corgi('Welshi', 'Cardigan Welsh', 'Short legs, big heart', 2),
+  Corgi('Pancake', 'Pembroke Welsh', 'The new pup on the scene', 0),
+]    
+
 def home(request):
   return HttpResponse('<h1>Corgi Collector</h1>')
 
 
 def about(request):
   return render(request, 'about.html')
+
+def corgis_index(request):
+  return render(request, 'corgis/index.html', { 'corgis': corgis })
