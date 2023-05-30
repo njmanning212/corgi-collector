20b0c949f195d52193d6557e56b7a12326321d67
Nicholas Manning <njmanning212@gmail.com>
2023-05-30 13:47:52 -0500

    update a tags in nav bar, correct spelling in name for index url name

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('corgis/', views.corgis_index, name='cargi-index'),
+  path('corgis/', views.corgis_index, name='corgi-index'),
   path('corgis/<int:corgi_id>/', views.corgis_detail, name='corgi-detail'),
 ]
