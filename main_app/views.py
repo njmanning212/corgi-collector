58a5c582190d240b2dcd02b90e79a628eed66863
Nicholas Manning <njmanning212@gmail.com>
2023-05-30 13:31:05 -0500

    create corgi model, remove hardcoded corgi data

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,7 @@
-from django.shortcuts import render
-
-class Corgi:
-  def __init__(self, name, type, description, age):
-    self.name = name
-    self.type = type
-    self.description = description
-    self.age = age
-
-corgis = [
-  Corgi('Toffee', 'Pembroke Welsh', 'All about the foot', 4),
-  Corgi('Bilbo', 'Pembroke Welsh', 'The best herder', 3),
-  Corgi('Welshi', 'Cardigan Welsh', 'Short legs, big heart', 2),
-  Corgi('Pancake', 'Pembroke Welsh', 'The new pup on the scene', 0),
-]    
+from django.shortcuts import render 
 
 def home(request):
   return render(request, 'home.html')
-
 
 def about(request):
   return render(request, 'about.html')
