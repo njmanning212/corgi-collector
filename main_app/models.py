8608a92898ca80bedc72028ce01bf49edb86d9aa
Nicholas Manning <njmanning212@gmail.com>
2023-05-30 15:27:37 -0500

    redirect user to corgi details once crogi created with get absolute url

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Corgi(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +9,6 @@ class Corgi(models.Model):
 
     def __str__(self):
       return self.name
+    
+    def get_absolute_url(self):
+      return reverse('corgi-detail', kwargs={'corgi_id': self.id})
