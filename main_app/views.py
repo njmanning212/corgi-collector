e3ea5a116ae4d77d48dae428a3d8c21b4cdc694e
Nicholas Manning <njmanning212@gmail.com>
2023-05-30 13:34:25 -0500

    import corgi model into views and use within corgi index

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render 
+from .models import Corgi
 
 def home(request):
   return render(request, 'home.html')
@@ -7,4 +8,5 @@ def about(request):
   return render(request, 'about.html')
 
 def corgis_index(request):
+  corgis = Corgi.objects.all()
   return render(request, 'corgis/index.html', { 'corgis': corgis })
