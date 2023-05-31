b7edcc7ace1e331f7ecb0a9fb058c1e5784e763f
Nicholas Manning <njmanning212@gmail.com>
2023-05-31 10:20:46 -0500

    add feeding model and MEALS tuples

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 class Corgi(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
@@ -12,3 +18,19 @@ class Corgi(models.Model):
     
     def get_absolute_url(self):
       return reverse('corgi-detail', kwargs={'corgi_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+      max_length=1,
+      choices=MEALS,
+      default=MEALS[0][0]
+    )
+
+    corgi = models.ForeignKey(Corgi, on_delete=models.CASCADE)
+
+    def __str__(self):
+      return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+      ordering = ['-date']
