75f619fa3ac97e0de0ad0f416072bae9c1dd8550
Nicholas Manning <njmanning212@gmail.com>
2023-05-31 14:51:33 -0500

    create, register, and migrate toy model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Corgi, Feeding
+from .models import Corgi, Feeding, Toy
 
 # Register your models here.
 admin.site.register(Corgi)
 admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,16 @@ MEALS = (
   ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+      return self.name
+
+    def get_absolute_url(self):
+      return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Corgi(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
