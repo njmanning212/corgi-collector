06f6cbb17291df62747714e03ffa6e5d3dfc7109
Nicholas Manning <njmanning212@gmail.com>
2023-05-31 10:22:15 -0500

    register feeding

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Corgi
+from .models import Corgi, Feeding
 
 # Register your models here.
 admin.site.register(Corgi)
+admin.site.register(Feeding)
