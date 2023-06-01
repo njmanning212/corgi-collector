82a5d8d2e2a1214d639bf64ef8abb13a039d99d5
Nicholas Manning <njmanning212@gmail.com>
2023-06-01 12:38:12 -0500

    display login on home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('corgis/', views.corgis_index, name='corgi-index'),
   path('corgis/<int:corgi_id>/', views.corgis_detail, name='corgi-detail'),
